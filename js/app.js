9d20eaaa766d4ed52455185e8b7d97b9b7aa3eac
secretrobotron <secretrobotron@gmail.com>
2017-06-14 01:18:39 -0400

    growl message cleanup

--- js/app.js
+++ js/app.js
@@ -177,7 +177,7 @@ function getRandomPhrase () {
 
 var growlFunctions = {
   chat: function (notification, topic, message) {
-    topic.innerHTML = 'New message from ' + getRandomWord('handle');
+    topic.innerHTML = 'Message from ' + getRandomWord('handle');
     notification.classList.add('chat');
     message.innerHTML = getRandomPhrase();
   },
@@ -190,7 +190,7 @@ var growlFunctions = {
   newFlamingEmail: function (notification, topic, message) {
     inboxCount++;
     notification.classList.add('mail');
-    topic.innerHTML = 'New mail from ' + getRandomWord('boss');
+    topic.innerHTML = getRandomWord('boss');
     message.innerHTML = 'URGENT ' + (getRandomWord('noun') + "S") .toUpperCase() + ' NEEDED';
   }
 };
